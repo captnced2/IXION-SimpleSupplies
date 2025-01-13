49c1bb6ea1adfaaeedb0eb3d94a5f6fb53960586
captnced <cedric.elsaesser1603@gmail.com>
2025-01-13 16:55:46 +0100

    Initial commit

---
+++ Patches.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BulwarkStudios.Stanford.Core.GameResources;
+using BulwarkStudios.Stanford.Core.GameStates;
+using BulwarkStudios.Stanford.Torus.Buildings;
+using BulwarkStudios.Stanford.Torus.Sectors;
+using HarmonyLib;
+using Random = System.Random;
+
+namespace SimpleSupplies;
+
+public class Patches
+{
+    internal static Dictionary<TorusSectorInstance, List<Building>> buildings = new();
+    internal static bool init;
+
+    [HarmonyPatch(typeof(Game), nameof(Game.SetState))]
+    public static class GameStatePatcher
+    {
+        public static void Postfix(Game.STATE state)
+        {
+            if (state == Game.STATE.PLAYING)
+            {
+                if (Plugin.helperPresent)
+                    if (!Plugin.getSettingValue())
+                        return;
+                if (init) return;
+                init = true;
+                var rn = new Random();
+                foreach (var dictionary in buildings)
+                {
+                    var resources = new List<ResourceData>();
+                    foreach (var b in dictionary.Value)
+                    {
+                        var newResource = true;
+                        foreach (var r in resources)
+                            if (r.type == b.resource.type)
+                                newResource = false;
+
+                        if (newResource)
+                            resources.Add(b.resource);
+                    }
+
+                    foreach (var r in resources)
+                    {
+                        var resourceBuildings = new List<Building>();
+                        foreach (var b in dictionary.Value)
+                            if (b.resource.type == r.type)
+                                resourceBuildings.Add(b);
+
+                        var newSupply = resourceBuildings[rn.Next(resourceBuildings.Count)];
+                        foreach (var b in resourceBuildings)
+                            if (!(b == newSupply))
+                            {
+                                newSupply.amount += b.amount;
+                                b.instance.availableStock.GetStored().Resources.RemoveAllResources();
+                            }
+
+                        newSupply.instance.availableStock.GetStored().Resources.SetResource(r.type, newSupply.amount);
+                    }
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(BuildingBehaviour), nameof(BuildingBehaviour.ApplyState))]
+    public static class BuildingInstancePatcher
+    {
+        public static void Postfix(BuildingBehaviour __instance)
+        {
+            if (Plugin.helperPresent)
+                if (!Plugin.getSettingValue())
+                    return;
+            if (__instance.Data.name.ToLower().Contains("supplies") ||
+                __instance.Data.name.ToLower().Contains("debris"))
+            {
+                var res = __instance.instance.availableStock.GetStored().Resources.GetListResources()._items.First();
+                var building = new Building(__instance.instance, res,
+                    __instance.instance.availableStock.GetStored().Resources.GetResourceCount(res));
+                foreach (var dictionary in buildings)
+                    if (dictionary.Key == __instance.instance.GetSector())
+                    {
+                        dictionary.Value.Add(building);
+                        return;
+                    }
+
+                var newList = new List<Building>();
+                newList.Add(building);
+                buildings.Add(__instance.instance.GetSector(), newList);
+            }
+        }
+    }
+
+    internal class Building(BuildingInstance Instance, ResourceData Resource, int Amount)
+    {
+        internal readonly BuildingInstance instance = Instance;
+        internal readonly ResourceData resource = Resource;
+        internal int amount = Amount;
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using BulwarkStudios.Stanford.Torus.Sectors;
+using HarmonyLib;
+using IMHelper;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+namespace SimpleSupplies;
+
+[BepInPlugin(Guid, Name, Version)]
+[BepInProcess("IXION.exe")]
+[BepInDependency("captnced.IMHelper", BepInDependency.DependencyFlags.SoftDependency)]
+public class Plugin : BasePlugin
+{
+    private const string Guid = "captnced.SimpleSupplies";
+    private const string Name = "SimpleSupplies";
+    private const string Version = "1.0.0";
+    internal new static ManualLogSource Log;
+    internal static SettingsHelper.BooleanSetting setting;
+    internal static bool helperPresent;
+
+    public override void Load()
+    {
+        Log = base.Log;
+        SceneManager.activeSceneChanged += (UnityAction<Scene, Scene>)sceneChangedListener;
+        var harmony = new Harmony(Guid);
+        harmony.PatchAll();
+        foreach (var patch in harmony.GetPatchedMethods())
+            Log.LogInfo("Patched " + patch.DeclaringType + ":" + patch.Name);
+        if (IL2CPPChainloader.Instance.Plugins.ContainsKey("captnced.IMHelper")) addHelperSetting();
+        Log.LogInfo("Loaded \"" + Name + "\" version " + Version + "!");
+    }
+
+    internal static void addHelperSetting()
+    {
+        helperPresent = true;
+        var section = new SettingsHelper.SettingsSection("SimpleSupplies");
+        setting = new SettingsHelper.BooleanSetting(section, "Enabled", "Enables/disables this mod", true, false);
+    }
+
+    internal static bool getSettingValue()
+    {
+        return setting.getValue();
+    }
+
+    internal static void sceneChangedListener(Scene current, Scene next)
+    {
+        if (SceneManager.GetActiveScene().name.Equals("MainMenu"))
+        {
+            Patches.init = false;
+            Patches.buildings = new Dictionary<TorusSectorInstance, List<Patches.Building>>();
+        }
+    }
+}
